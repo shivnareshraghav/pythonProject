d677723247fb8faa305267b5a9c45567d4d047f3
shivnareshraghav <shivnr015@gmail.com>
2022-08-26 18:40:11 +0530

    Initial commit

---
+++ d.py
@@ -0,0 +1,141 @@
+# a=9
+# b=8
+# c=9
+# d=a+b+c
+# print(d)
+#
+# bool
+#
+# a=8
+# b=7
+# c=a<b
+# print(c)
+#
+# b=6
+# c=4
+# d=b>c
+# print(d)
+# a="654"
+# print(type (a))
+# a1=123
+# a2=123
+# print(a1+a2)
+# a=90
+# b=60
+# print(a+b)
+# a=80
+# b=60
+# print(b-a)
+# a=62.589
+#
+# print(type(a))
+#
+# age=23
+# if age>=18:
+    # print('adult')
+
+
+#
+# else:
+    # print("child")
+#
+#
+# a=7
+# b=5
+# c=7-5
+# print(c)
+#
+#
+# purchage_value = int(input("please enter your bill amount: "))
+# if purchage_value>=1000:
+#     bill = purchage_value-100
+#     print("Your bill after discount: " + str(bill))
+# else:
+#     print("Your bill is: " + str(purchage_value))
+#
+#
+# 10_percentage_of_x = x*10/100
+#
+# # purchasevalue=int(input("please enter your amount: "))
+# # if purchasevalue>=1000:
+# #     percentage_of_purchasevalue =purchasevalue*10/100
+# #     bill= purchasevalue-percentage_of_purchasevalue
+# #     print( "dear your bill after discount is this: " + str(bill))
+# # else:
+# #     print( "your bill is : " + str(purchasevalue))
+#
+#
+#
+# socks=50
+# tshirt=350
+# trouser=800
+# belt=100
+# purchasevalue=int(input("enter your amount: "))
+# if purchasevalue>=1000:
+#
+# discount=purchasevalue-percentage_of_purchasevalue*10/100
+# bill=purchasevalue-discount
+# print("your bill after discount is this :" +str(purchasevalue))
+
+
+
+
+
+# a = '200'
+# b= 100
+# c = a-b
+# print(c)
+
+#
+# list1=[1,2,3,4,5,6,7]
+# index=1
+# for item in list1:
+#     r = round(len(list1)/2)
+#     if r ==  index:
+#         break
+#     v=item*list1[-index]
+#     index=index+1
+#     print(v)
+# list=["a","b","c","d"]
+# v = int(input("dalo: "))
+# a=list[v-1]
+# print(a)
+# output=["SoNu","RaHuL"]
+# list1=['Sonu','Rahul','Nagar']
+# list2=[]
+# for item in list1:
+#     i=1
+#     v=''
+#     for itm in item:
+#         if i%2==0:
+#            v=v+itm
+#         else:
+#            v=v+itm.upper()
+#            i=i+1
+#     list2.append(v)
+# print(list2)
+# list1=[Rahul]
+# print("this is double \\\\ backslash")
+# print("these are /\\/\\/\\/\\/\\ mountains")
+# print("he is \tawesome")
+# print("\\\" \\n \\t \\\\'")
+# print("lineA\n lineB")
+# print(r'lineA\nlineB')
+# print('\U0001F923')
+# print('\U0001F61C')
+# print('abhishek* "3"')
+# r= "abhishek "*3
+#s=3
+#d = r*s
+#d=('abhishek *"3"')
+# print(r)
+
+
+
+
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,19 @@
+list1 = [2,4,5,6,7,8,9,11]
+list1.append(12)
+
+for itm in list1:
+    if itm == 2:
+        itm = itm * 2
+        print(itm)
+    elif itm == 11:
+        itm = itm * 2
+        print(itm)
+    elif itm == 9:
+        itm = itm * 2
+        print(itm)
+    elif itm == 12:
+        itm = itm * 2
+        print(itm)
+        print (itm)
+
+
